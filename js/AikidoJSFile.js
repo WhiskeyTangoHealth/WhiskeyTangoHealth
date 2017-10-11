227f36b0222a2f6ebebd5ed1e9a9b7e55695b7a5
Clydezilla <sacredforgeaikido@gmail.com>
2017-10-11 13:19:57 -0500

    Add files via upload

--- js/AikidoJSFile.js
+++ js/AikidoJSFile.js
@@ -5,7 +5,7 @@
   var displayId = ['Beginner Classes \n  M W F Evenings 6:00 P.M. - 7:00 P.M.',
                    'Advanced Classes M W F Evenings \n  7:00 P.M. – 8:00 P.M.',
                    'Chuck Norris has now been notified of your claim.\n    Don\'t bother running, you\'ll just die tired.',
-                   'Please make a selection first']
+                   'Please make a selection first'];
 
   function displayCheck(){
      if (document.getElementById('1').checked) {
@@ -19,10 +19,49 @@
      }
    }
 
-   var thing = document.getElementById('submitButton2');
+   var thing = document.getElementById('submitButton');
    thing.addEventListener("click", displayCheck, false);
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  JS for little Samurai
+  var littleSamurai = "Class times for Little Samurai:\n\nWednesdays 4:00P.M. to 5:30P.M.\n\nSaturday from 9:00 A.M. to 10:30 A.M";
 
+  function kidsCheck(){
+    alert(littleSamurai);
+  }
+
+  var thing1 = document.getElementById('submitButton2');
+  thing1.addEventListener("click", kidsCheck, false);
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~************ END CLASS TIMES *****
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  JS for Modal Form Validation
+document.getElementById('notOfAge').checked = true; // sets form to minor by default
+function modalValidation(){
+  //alert("hey")
+  try {
+    var email = document.getElementById('email').value;
+    var userName = document.getElementById('name').value;
+    var telNum = document.getElementById('tel').value;
+    if (userName === "" || userName === null || email === "" || email === null || telNum === "" || telNum === null) {
+        throw "Incomplete fields.";
+    } else if (userName.length <= 5) {
+        throw "Get your info right\nor Chuck Norris wil find you.";
+    } else if (document.getElementById('notOfAge').checked) {
+        throw "Verify your age right meow."
+    }
+
+  } catch (e) {
+      window.alert(e);
+      return false;
+  } finally {
+    document.getElementById('name').value = "";
+    document.getElementById('email').value = "";
+    document.getElementById('tel').value = "";
+    document.getElementById('notOfAge').checked = true;
+  }
+}
+
+  var modalSubmit = document.getElementById('modalSubmit');
+  modalSubmit.addEventListener("click", modalValidation, false);
 //                  *** sensei's corner/zen sayings ***
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ set a timer
    var mySuperAwesomeTimer = setInterval(zenSaying, 7000);
